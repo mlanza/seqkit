ef690504e3b9a829fb2d94a6eff91dbbfa6f6717
Mario T. Lanza <mario@doesideas.com>
2025-12-31 13:05:25 -0500

    cross-platform support

---
+++ bin/nt.bat
@@ -0,0 +1,2 @@
+@echo off
+deno run --allow-run --allow-read --allow-env "%~dp0nt" %*
--- bin/nt.d/wikilinks.ps1
+++ bin/nt.d/wikilinks.ps1
@@ -3,4 +3,4 @@ $input |
   Select-String -Pattern '\[\[([^\]\r\n]+)\]\]' -AllMatches |
   ForEach-Object {
     $_.Matches | ForEach-Object { $_.Groups[1].Value }
-  } | nt seen
+  }
