268327e52c24a12a1018b7f39d861005beb077af
Mario T. Lanza <mario@doesideas.com>
2026-01-09 12:34:18 -0500

    save

--- bin/nt.d/post.ps1
+++ bin/nt.d/post.ps1
@@ -1,45 +1,59 @@
 #!/usr/bin/env pwsh
 
-# Post - Process stdin through serial then pipe to update, or handle properties only
+# Post - Process stdin or argument through serial then pipe to update, or handle properties only
 # Usage: echo "content" | nt post [--prepend] [--debug] [--overwrite] [--add "key=value"...] <page_name>
+# Usage: nt post [--prepend] [--debug] [--overwrite] [--add "key=value"...] <page_name> "[content]"
 # Usage: nt post [--add "key=value"...] <page_name>
 
-# Check if stdin has content
-$hasStdin = $false
-try {
-    $stdinAvailable = [Console]::IsInputRedirected
-    if ($stdinAvailable) {
-        $inputs = [Console]::In.ReadToEnd()
-        $hasStdin = -not [string]::IsNullOrWhiteSpace($inputs)
-    }
-} catch {
-    # Fallback if stdin check fails
-    $hasStdin = $false
-}
-
-# Separate prop arguments from other arguments
+# --- Argument and Input Parsing ---
+$linesToProcess = @()
 $propArgs = @()
 $otherArgs = @()
 $pageName = $null
+$pageNameFound = $false
 
-for ($i = 0; $i -lt $args.Count; $i++) {
-    if ($args[$i] -eq "--add" -and $i + 1 -lt $args.Count) {
-        $propArgs += $args[$i + 1]
-        $i++
-    } elseif (-not $pageName) {
-        $pageName = $args[$i]
-    } else {
-        $otherArgs += $args[$i]
+# Check for stdin first
+if ([Console]::IsInputRedirected) {
+    $stdinText = [Console]::In.ReadToEnd().Trim()
+    if (-not [string]::IsNullOrWhiteSpace($stdinText)) {
+        $linesToProcess += ($stdinText -split '\r?\n')
     }
 }
 
-# Call nt prop first if any prop arguments provided
+# Parse command-line arguments
+$i = 0
+while ($i -lt $args.Count) {
+    $arg = $args[$i]
+    if ($arg -eq "--add" -and $i + 1 -lt $args.Count) {
+        $propArgs += $args[$i + 1]
+        $i += 2
+        continue
+    }
+
+    if ($arg.StartsWith("-")) {
+        $otherArgs += $arg
+        $i++
+        continue
+    }
+
+    if (-not $pageNameFound) {
+        $pageName = $arg
+        $pageNameFound = $true
+    } else {
+        # The content argument.
+        # Interpret literal '\n' as actual newlines.
+        $linesToProcess += (($arg.Replace('\n', "`n")) -split '\r?\n')
+    }
+    $i++
+}
+
+# Handle property arguments if provided
 if ($propArgs.Count -gt 0) {
     $propAddArgs = $propArgs | ForEach-Object { "--add", $_ }
     & nt prop $pageName @propAddArgs
 }
 
-# If stdin was provided, process it through serial then update
-if ($hasStdin) {
-    $inputs | & nt blocked | & nt serial | & nt update $otherArgs $pageName
+# Unified Pipeline: Process all collected lines from any source
+if ($linesToProcess.Count -gt 0) {
+    $linesToProcess | & nt blocked | & nt serial | & nt update $otherArgs $pageName
 }
